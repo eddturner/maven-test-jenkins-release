eeaf0185737bfcb13700cfdcfb32ea3caaedfe8b
Edward Turner <eddturner@ebi.ac.uk>
2017-06-28 15:45:00 +0100

    changed message in servlet

--- core/src/main/java/com/jamesward/HelloServlet.java
+++ core/src/main/java/com/jamesward/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello, world".getBytes());
+        out.write("hello, world, may you be well".getBytes());
         out.flush();
         out.close();
     }
